f7bd65514db27ecb2a8962c82eaa09fbd2dfceee
OkibeAbang <113947911+OkibeAbang@users.noreply.github.com>
2023-02-12 21:07:37 -0700

    Update ex1.5.py
    Updated timing code

--- ex1.5.py
+++ ex1.5.py
@@ -1,7 +1,6 @@
 #Timing the original code
 import timeit
-#Fibonacci function using recursion
-
+#Fibonacci function using recursion 
 
 
 
@@ -10,29 +9,27 @@ code_to_test= """def func(n):
         return n
     else:
         return func(n-1) + func(n-2)
-func(0)
+func(35)
 """
- 
+
 elapsed_time = timeit.timeit(code_to_test, number=1)
-print("Time elapsed original code: ", elapsed_time, "seconds")
-
-
+print("Time elapsed: ", elapsed_time, "seconds")
 
 
 
 
 #Timig the improved code
-#Using memoization to implement the code
-code_to_test= """term = [0 for i in range(1000)]
  
-def fib(n):
+#Using memoization to implement the code
+code_to_test= """def fib(n, memo={}):
     if n == 0 or n == 1:
         return n
-    else:
-        term[n] = fib(n-1)+fib(n-2)
-        return term[n]
-fib(0)
+    if n in memo:
+        return memo[n]
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+fib(35)
 """
- 
+
 elapsed_time = timeit.timeit(code_to_test, number=1)
-print("Time elapsed improved code: ", elapsed_time, "seconds")
+print("Time elapsed: ", elapsed_time, "seconds")
